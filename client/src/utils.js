e5aa94980890d0b2e173dcb82e69ec189f347fc9
Simon Cordova <simoncordova@Simons-MacBook-Pro.local>
2025-07-03 15:56:23 +0100

    Add missing getInitials function

---
+++ client/src/utils.js
@@ -0,0 +1,7 @@
+export const getInitials = (name) => {
+  if (!name) return '';
+  const names = name.split(' ');
+  return names.length > 1
+    ? `${names[0][0]}${names[names.length - 1][0]}`
+    : names[0][0] || '';
+};
