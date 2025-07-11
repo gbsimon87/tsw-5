be350684ea640d9c7ced1ab34b8722e55788beba
Simon Cordova <simoncordova@Simons-MacBook-Pro.local>
2025-07-12 00:15:58 +0100

    Make logo circular with 50 percent border radius

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -61,7 +61,7 @@ function NavBar() {
           </div>
           <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
             <NavLink to="/" className="flex shrink-0 items-center text-2xl font-bold text-white no-underline" aria-label="Home">
-              <img src={tswLogo} alt="TSW" className="h-8" aria-label='TSW Logo' />
+              <img src={tswLogo} alt="TSW" className="h-8 rounded-full" aria-label="TSW Logo" />
             </NavLink>
             <div className="hidden sm:ml-6 sm:block">
               <div className="flex space-x-4">
