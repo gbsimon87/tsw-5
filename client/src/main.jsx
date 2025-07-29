e44756d27604080a02928053f8f25807217094f2
Yibran Simon <y.simon.cordova@gmail.com>
2025-07-30 00:02:30 +0100

    Refix GA env variables

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,24 +6,6 @@ import App from './App.jsx';
 import Modal from 'react-modal';
 import { BrowserRouter } from 'react-router-dom';
 
-// Initialize GA4
-// const measurementId = import.meta.env.VITE_REACT_APP_GA_MEASUREMENT_ID;
-// if (measurementId) {
-//   const script = document.createElement('script');
-//   script.async = true;
-//   script.src = `https://www.googletagmanager.com/gtag/js?id=${measurementId}`;
-//   document.head.appendChild(script);
-
-//   const inlineScript = document.createElement('script');
-//   inlineScript.innerHTML = `
-//     window.dataLayer = window.dataLayer || [];
-//     function gtag(){dataLayer.push(arguments);}
-//     gtag('js', new Date());
-//     gtag('config', '${measurementId}');
-//   `;
-//   document.head.appendChild(inlineScript);
-// }
-
 createRoot(document.getElementById('root')).render(
   // <StrictMode>
     <BrowserRouter>
